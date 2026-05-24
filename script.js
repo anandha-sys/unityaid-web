471bb05ac294229a94d1d8a846c9aaf31ee8a098
anandha-sys <krishnanhanandha45@gmail.com>
2026-05-24 15:19:00 +0530

    Refactor countdown timer and add user notification
    Removed countdown timer functionality and added a message alert.

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-const launchDate = new Date("December 31, 2026 00:00:00").getTime();
+function showMessage(){
 
-const timer = setInterval(function(){
+  alert("Thank you for supporting UnityAid ❤️");
 
-  const now = new Date().getTime();
-
-  const distance = launchDate - now;
-
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24))
-    / (1000 * 60 * 60)
-  );
-
-  const minutes = Math.floor(
-    (distance % (1000 * 60 * 60))
-    / (1000 * 60)
-  );
-
-  const seconds = Math.floor(
-    (distance % (1000 * 60))
-    / 1000
-  );
-
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-
-},1000);
-
-function notifyUser(){
-  alert("Thanks for joining UnityAid ❤️");
 }
